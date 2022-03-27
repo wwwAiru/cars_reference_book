e1aa45e5b93d493d2203093d0ce94bb0782aac94
Andrey Golikov <z-a-t-o@yandex.ru>
2022-03-27 22:00:10 +0300

    styles + alerts

--- form.py
+++ form.py
@@ -8,6 +8,11 @@ class CarsForm(FlaskForm):
     car_model = StringField('Модель: ', validators=[InputRequired()])
     category = StringField('Категория транспортного средства: ', validators=[InputRequired()])
     car_license_plate = StringField('Государственный номер: ', validators=[InputRequired()])
-    car_type = StringField('Тип транспортного средства: ', validators=[InputRequired()])
+    car_type = SelectField('Тип транспортного средства: ', choices=[('Легковой', 'Легковой'),
+                                                                    ('Грузовой', 'Грузовой'),
+                                                                    ('Микроавтобус ', 'Микроавтобус '),
+                                                                    ('Автобус', 'Автобус'),
+                                                                    ('Автомобиль - тягач', 'Автомобиль - тягач')
+                                                                    ])
     release_year = DateField('Год выпуска: ', validators=[InputRequired()])
     trailer = SelectField('Наличие прицепа: ', choices=[('Нет', 'Нет'), ('Да', 'Да')])
